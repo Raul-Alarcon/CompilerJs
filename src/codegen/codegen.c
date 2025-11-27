7a4bd9fd19f2bf3bfd61f73a5528cf215095cc18
marcosherna <marcosxdf6@gmail.com>
2025-11-27 09:58:41 -0600

    fix: separate responsability

--- src/codegen/codegen.c
+++ src/codegen/codegen.c
@@ -11,128 +11,119 @@ char *newLabel()
     sprintf(label, "L%d", labelCount++);
     return label;
 }
+int newTemp() { return temp++; }
 
-void generate_ir(ASTNode *node)
-{
-    if (!node)
-        return;
+int generate_expr(ASTNode* node) {
+    switch (node->type) {
 
-    switch (node->type)
-    {
-    case NODE_STMTLIST:
-    {
-        ASTNode *cur = node->next;
-        while (cur)
-        {
-            generate_ir(cur);
+    case NODE_NUM: {
+        int t = newTemp();
+        printf("t%d = %d\n", t, node->num);
+        return t;
+    }
+
+    case NODE_ID: {
+        int t = newTemp();
+        printf("t%d = %s\n", t, node->id);
+        return t;
+    }
+
+    case NODE_BINOP: {
+        int l = generate_expr(node->left);
+        int r = generate_expr(node->right);
+        int t = newTemp();
+        printf("t%d = t%d %s t%d\n", t, l, node->op, r);
+        return t;
+    }
+
+    default:
+        printf("ERROR: expresión inválida\n");
+        exit(1);
+    }
+}
+
+void generate_stmt(ASTNode* node) {
+
+    switch (node->type) {
+
+    case NODE_ASSIGN: {
+        int t = generate_expr(node->left);    
+        printf("STORE %s <- t%d\n", node->id, t);
+        break;
+    }
+
+    case NODE_RETURN: {
+        int t = generate_expr(node->left);
+        printf("RETURN t%d\n", t);
+        break;
+    }
+
+    case NODE_IF: {
+        char* Ltrue = newLabel();
+        char* Lend = newLabel();
+
+        int cond = generate_expr(node->left);
+
+        printf("IF t%d GOTO %s\n", cond, Ltrue);
+        printf("GOTO %s\n", Lend);
+
+        printf("%s:\n", Ltrue);
+        generate_stmt(node->right);
+
+        printf("%s:\n", Lend);
+        break;
+    }
+
+    case NODE_FOR: {
+        char *Lstart = newLabel();
+        char *Lbody  = newLabel();
+        char *Lstep  = newLabel();
+        char *Lend   = newLabel();
+
+        if (node->init)
+            generate_stmt(node->init);
+
+        printf("%s:\n", Lstart);
+
+        if (node->cond) {
+            int cond = generate_expr(node->cond);
+            printf("IF t%d GOTO %s\n", cond, Lbody);
+            printf("GOTO %s\n", Lend);
+        } else {
+            printf("GOTO %s\n", Lbody);
+        }
+
+        printf("%s:\n", Lbody);
+        generate_stmt(node->body);
+
+        printf("%s:\n", Lstep);
+        generate_stmt(node->step);
+
+        printf("GOTO %s\n", Lstart);
+        printf("%s:\n", Lend);
+
+        break;
+    }
+
+    case NODE_BLOCK:
+        generate_stmt(node->left);
+        break;
+
+    case NODE_STMTLIST: {
+        ASTNode* cur = node->next;
+        while (cur) {
+            generate_stmt(cur);
             cur = cur->next;
         }
         break;
     }
 
-    case NODE_ASSIGN:
-    {
-        generate_ir(node->left);
-        printf("STORE %s <- t%d\n", node->id, temp - 1);
-        break;
-    }
-
-    case NODE_NUM:
-    {
-        printf("t%d = %d\n", temp++, node->num);
-        break;
-    }
-
-    case NODE_BINOP:
-    {
-        generate_ir(node->left);
-        generate_ir(node->right);
-        int r = temp - 1;
-        int l = temp - 2;
-        printf("t%d = t%d %s t%d\n", temp++, l, node->op, r);
-        break;
-    }
-
-    case NODE_RETURN:
-    {
-        generate_ir(node->left);
-        printf("RETURN t%d\n", temp - 1);
-        break;
-    }
-
-    case NODE_FUNCTION:
-    {
-        printf("FUNC %s:\n", node->id);
-        generate_ir(node->left);
-        printf("ENDFUNC\n");
-        break;
-    }
-
-    case NODE_BLOCK:
-        generate_ir(node->left);
-        break;
-
-    case NODE_IF:
-    {
-        char *Ltrue = newLabel();
-        char *Lend = newLabel();
-
-        generate_ir(node->left);
-        printf("IF t%d GOTO %s\n", temp - 1, Ltrue);
-        printf("GOTO %s\n", Lend);
-
-        printf("%s:\n", Ltrue);
-        generate_ir(node->right);
-
-        printf("%s:\n", Lend);
-        break;
-    }
-    case NODE_FOR:
-    {
-        char *Lstart = newLabel();
-        char *Lbody = newLabel();
-        char *Lstep = newLabel();
-        char *Lend = newLabel();
- 
-        if (node->init)
-        {
-            generate_ir(node->init);
-        }
- 
-        printf("%s:\n", Lstart);
- 
-        if (node->cond)
-        {
-            generate_ir(node->cond);
-            printf("IF t%d GOTO %s\n", temp - 1, Lbody);
-            printf("GOTO %s\n", Lend);
-        }
-        else
-        { 
-            printf("GOTO %s\n", Lbody);
-        }
- 
-        printf("%s:\n", Lbody);
-
-        if (node->body)
-        {
-            generate_ir(node->body);
-        }
- 
-        printf("%s:\n", Lstep);
-
-        if (node->step)
-        {
-            generate_ir(node->step);
-        }
- 
-        printf("GOTO %s\n", Lstart);
- 
-        printf("%s:\n", Lend);
-        break;
-    }
-
     default:
-        break;
+        printf("ERROR stmt\n");
+        exit(1);
     }
+}
+ 
+void generate_ir(ASTNode* root) {
+    generate_stmt(root);
 }
